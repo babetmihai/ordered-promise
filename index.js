64346d71c256b00e26ee56a350bea18d6b880290
Mihai Babet <babetmihai@users.noreply.github.com>
2019-02-15 23:35:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-
-
-const wrapper = (fn) => {
+const queueWrapper = (fn) => {
   const promiseQueue = []
   return (...args) => new Promise((resolve, reject) => {
     const lastPromise = promiseQueue[promiseQueue.length - 1]
-    let resolver
-    promiseQueue.push(new Promise((queueResolve) => {
-      resolver = queueResolve
+    let queueResolve
+    promiseQueue.push(new Promise((_queueResolve) => {
+      queueResolve = _queueResolve
     }))
     
     if (lastPromise) {
-      return fn(...args)
+      fn(...args)
         .then((res) => lastPromise.then(() => {
-          resolver()
           promiseQueue.shift()
+          queueResolve()
           resolve(res)
         }))
         .catch((error) => lastPromise.then(() => {
-          resolver()
           promiseQueue.shift()
+          queueResolve()
           reject(error)
         }))
     } else {
-      return fn(...args)
+      fn(...args)
         .then((res) => {
-          resolver()
           promiseQueue.shift()
+          queueResolve()
           resolve(res)
         })
         .catch((error) => {
-          resolver()
-          promiseQueue.shift()
+          promiseQueue.shift() 
+          queueResolve()
           reject(error)
         })
       }
     })
   }
 
-const fetch = (delay) => new Promise((resolve) => setTimeout(() => {
+const delayedLog = (delay) => new Promise((resolve) => setTimeout(() => {
   resolve(delay)
 }, delay))
 
 
-const wrappedFetch = wrapper(fetch)
+const orderedLog = queueWrapper(delayedLog)
 
-wrappedFetch(3000).then(console.log)
-wrappedFetch(2001).then(console.log)
-wrappedFetch(1000).then(console.log)
+orderedLog(1005).then(console.log).then(() => orderedLog(105).then(console.log))
+orderedLog(2001).then(console.log).then(() => orderedLog(1002).then(console.log))
+orderedLog(1000).then(console.log).then(() => orderedLog(101).then(console.log))
+orderedLog(1003).then(console.log)
