8cf7fe4f659717c66c4dc98a0719ccb63c83f1d0
Mihai Babet <babetmihai@users.noreply.github.com>
2019-02-16 11:54:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
-
 const queueWrapper = (fn) => {
   const promiseQueue = []
   return (...args) => new Promise((resolve, reject) => {
-    const lastPromise = promiseQueue[promiseQueue.length - 1]
+    const lastPromise = promiseQueue[promiseQueue.length - 1] || Promise.resolve()
     let queueResolve
-    promiseQueue.push(new Promise((_queueResolve) => {
-      queueResolve = _queueResolve
+    promiseQueue.push(new Promise((_resolve) => {
+      queueResolve = _resolve
     }))
     
-    if (lastPromise) {
-      fn(...args)
-        .then((res) => lastPromise.then(() => {
-          promiseQueue.shift()
-          queueResolve()
-          resolve(res)
-        }))
-        .catch((error) => lastPromise.then(() => {
-          promiseQueue.shift()
-          queueResolve()
-          reject(error)
-        }))
-    } else {
-      fn(...args)
-        .then((res) => {
-          promiseQueue.shift()
-          queueResolve()
-          resolve(res)
-        })
-        .catch((error) => {
-          promiseQueue.shift() 
-          queueResolve()
-          reject(error)
-        })
-      }
+    fn(...args)
+      .then((res) => lastPromise.then(() => {
+        promiseQueue.shift()
+        queueResolve()
+        resolve(res)
+      }))
+      .catch((error) => lastPromise.then(() => {
+        promiseQueue.shift()
+        queueResolve()
+        reject(error)
+      }))
     })
   }
 
 // test
-const delayedLog = (delay) => new Promise((resolve) => setTimeout(() => {
+const logDelay = (delay) => new Promise((resolve) => setTimeout(() => {
   resolve(delay)
 }, delay))
 
 
-const orderedLog = queueWrapper(delayedLog)
+const orderedLog = queueWrapper(logDelay)
 
 orderedLog(1005).then(console.log).then(() => orderedLog(105).then(console.log))
 orderedLog(2001).then(console.log).then(() => orderedLog(1002).then(console.log))
