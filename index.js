a0e9c765392619d39dd8873ae93f88638d5425dc
Mihai Babet <babetmihai@users.noreply.github.com>
2019-02-16 12:59:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const queueWrapper = (fn) => {
-  const promiseQueue = []
+const orderedPromise = (fn) => {
+  let lastPromise = Promise.resolve()
   return (...args) => new Promise((resolve, reject) => {
-    const lastPromise = promiseQueue[promiseQueue.length - 1] || Promise.resolve()
-    let queueResolve
-    promiseQueue.push(new Promise((_resolve) => {
-      queueResolve = _resolve
-    }))
+    const _lastPromise = lastPromise
+
+    let lastResolve
+    lastPromise = new Promise((_lastResolve) => {
+      lastResolve = _lastResolve
+    })
 
     fn(...args)
-      .then((res) => lastPromise.then(() => {
-        promiseQueue.shift()
-        queueResolve()
-        resolve(res)
-      }))
-      .catch((error) => lastPromise.then(() => {
-        promiseQueue.shift()
-        queueResolve()
-        reject(error)
-      }))
+      .then((res) => _lastPromise.then(() => resolve(res)))
+      .catch((error) => _lastPromise.then(() => reject(error)))
+      .finally(() => {
+        lastResolve()
+        if (lastPromise === _lastPromise) lastPromise = Promise.resolve()
+      })
     })
 }
 
 // example
 const delay = (time) => new Promise((resolve) => setTimeout(() => resolve(time), time))
-const orderedLog = queueWrapper(delay)
+const orderedLog = orderedPromise(delay)
 
 orderedLog(100).then(console.log)
 orderedLog(2000).then(console.log)
 orderedLog(1000).then(console.log)
 orderedLog(500).then(console.log)
+delay(300).then(() => orderedLog(450).then(console.log))
+delay(400).then(() => orderedLog(250).then(console.log))
